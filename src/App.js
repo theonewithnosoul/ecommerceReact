cf0ff14a11435871dd4f2f211bbf2e68bb3dc8dc
Lucia leon <leon.mmlucia@gmail.com>
2021-05-14 18:40:06 -0300

    itemListContainer

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
 			<header>
 				<Navbarr />
 			</header>
-			<ItemListContainer/>
+			<ItemListContainer buyButton=' Comprar '/>
 		</body>
 
 	);
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,16 +1,30 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Uno from '../components/images/stock/1.png';
 
 
-const ItemListContainer = () =>{
-   return(
+
+const ItemListContainer = ({buyButton}) => {
+  return (
     <Container>
-    <Row>
-      <Col>1 of 1</Col>
-    </Row>
-  </Container>
-   )
+      <Row>
+        <Col><Card style={{ width: '18rem' }}>
+          <Card.Img  variant="top" src={Uno} />
+          <Card.Body>
+            <Card.Title>Producto</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk of
+              the card's content.
+            </Card.Text>
+            <Button variant="primary">{buyButton}</Button>
+          </Card.Body>
+        </Card></Col>
+      </Row>
+    </Container>
+  )
 }
 export default ItemListContainer;
